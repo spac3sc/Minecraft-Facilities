6e6d77b3c9675ac4f7ad7d788ff07caaea63d2c6
YukiSyaf <168244699+YukiSyaf@users.noreply.github.com>
2024-08-02 12:02:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Minecraft Facility</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial scale=1">
-    <link rel="stylesheet" href="">
-    <link rel="stylesheet" href="">
-    <link rel="stylesheet" href"">
-    <style> h1,h2,h3,h4,h5,h6 {font-family: "Oswald"} body {font-family: "Open Sans"} </style>
-  </head>
-  <body class="w3-light-grey">
-   <!-- Navigation bar with social media icons -->
-   <div class="w3-bar w3-black w3-hide-small">
-     <a href="" class="w3-bar-item w3-button">
-</html>
+
