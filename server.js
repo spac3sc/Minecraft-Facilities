7fbb221739f80bfbb5c959b30af2a1bf03910840
spac3sc <darealeljantmg6@gmail.com>
2024-08-24 20:37:50 +0800

    Initial commit

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-import { createServer } from 'node:http';
-const server = createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World!\n');
-});
-// starts a simple http server locally on port 3000
-server.listen(3000, '127.0.0.1', () => {
-  console.log('Listening on 127.0.0.1:3000');
+const http = require('http');
+const app = require('./app');
+
+const port = process.env.PORT || 5000;
+const server = http.createServer.apply(app);
+
+server.listen(port, () => {
+  console.log('Started on port ${port}');
 });
